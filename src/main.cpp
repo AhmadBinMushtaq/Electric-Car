68006a1aeb05128aacc1ee2ac67da8c4bd2424fd
Ahmad Mushtaq <ahmadbinmushtaq99@gmail.com>
2022-12-01 19:46:44 +0500

    Modified ESC part in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,32 @@
 #include <Arduino.h>
 #include <ESC.h>
 
-static const int ESCPin1 = 13;
-static const int ESCPin2 = 12;
-static const int ESCPin3 = 14;
-static const int ESCPin4 = 27;
+ESC motor1(12);
+ESC motor2(13);
+ESC motor3(14);
+ESC motor4(15);
 
-ESC ESC1;
-ESC ESC2;
-ESC ESC3;
-ESC ESC4;
 
 void setup() {
     Serial.begin(115200);
-    ESC1.attach(ESCPin1);
-    ESC2.attach(ESCPin2);
-    ESC3.attach(ESCPin3);
-    ESC4.attach(ESCPin4);
 }
 
 void loop() {
     for(int speed = 0; speed <= 100; speed++) {
-        ESC1.write(speed);
-        ESC2.write(180-speed);
-        ESC3.write(speed);
-        ESC4.write(speed);
+        motor1.write(speed);
+        motor2.write(speed);
+        motor3.write(speed);
+        motor4.write(speed);
         Serial.println(speed);
         delay(40);
     }
     delay(3000);
 
     for(int speed = 100; speed >= 0; speed--) {
-        ESC1.write(speed);
-        ESC2.write(100-speed);
-        ESC3.write(speed);
-        ESC4.write(speed);
+        motor1.write(speed);
+        motor2.write(speed);
+        motor3.write(speed);
+        motor4.write(speed);
         Serial.println(speed);
         delay(40);
     }
