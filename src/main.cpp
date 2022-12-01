aad6bf94980924d31de0798b5ea592aeb8083752
Ahmad Mushtaq <ahmadbinmushtaq99@gmail.com>
2022-12-01 19:57:31 +0500

    Added Current & Voltage Sensor to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <Arduino.h>
 #include <ESC.h>
+#include <ACS712.h>
+#include <Voltage_Sensor.h>
 
 ESC motor1(12);
 ESC motor2(13);
 ESC motor3(14);
 ESC motor4(15);
 
+ACS712 acs1(1,1,1);
+ACS712 acs2(1,1,1);
+ACS712 acs3(1,1,1);
+ACS712 acs4(1,1,1);
+
+Voltage_Sensor voltage(1,1);
 
 void setup() {
     Serial.begin(115200);
