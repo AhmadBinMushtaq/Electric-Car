e6b75f2ee3cbf405ee801a2776e9b261e9e5636f
Ahmad Mushtaq <ahmadbinmushtaq99@gmail.com>
2022-12-01 11:08:38 +0500

    Added ESC Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <ESC.h>
+
+static const int ESCPin1 = 13;
+static const int ESCPin2 = 12;
+static const int ESCPin3 = 14;
+static const int ESCPin4 = 27;
+
+ESC ESC1;
+ESC ESC2;
+ESC ESC3;
+ESC ESC4;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    ESC1.attach(ESCPin1);
+    ESC2.attach(ESCPin2);
+    ESC3.attach(ESCPin3);
+    ESC4.attach(ESCPin4);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    for(int speed = 0; speed <= 100; speed++) {
+        ESC1.write(speed);
+        ESC2.write(180-speed);
+        ESC3.write(speed);
+        ESC4.write(speed);
+        Serial.println(speed);
+        delay(40);
+    }
+    delay(3000);
+
+    for(int speed = 100; speed >= 0; speed--) {
+        ESC1.write(speed);
+        ESC2.write(100-speed);
+        ESC3.write(speed);
+        ESC4.write(speed);
+        Serial.println(speed);
+        delay(40);
+    }
+    delay(3000);
 }
