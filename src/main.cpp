4a800afa56e73acb711dcd8dcf43cc3299da3bcd
Ahmad Mushtaq <ahmadbinmushtaq99@gmail.com>
2022-12-13 15:07:23 +0500

    Created Separate Header file for definitions

--- src/main.cpp
+++ src/main.cpp
@@ -3,69 +3,7 @@
 #include <ACS712.h>
 #include <Voltage_Sensor.h>
 #include <GPS.h>
-
-
-// Pin Definitions
-
-#define RXPin1 2
-#define RXPin2 4
-#define RXPin3 5
-#define RXPin4 18
-#define RXPin5 19
-
-#define VoltmeterPin 15
-#define MaxVoltage 25
-
-#define SteeringPin 23
-
-#define ACS1Pin 35
-#define ACS1Slope 9.04405
-#define ACS1Offset -14.3177
-
-#define ACS2Pin 34
-#define ACS2Slope 9.04405
-#define ACS2Offset -15.3177
-
-#define ACS3Pin 26
-#define ACS3Slope 22.3971
-#define ACS3Offset -37.2174
-
-#define ACS4Pin 25
-#define ACS4Slope 22.3757
-#define ACS4Offset -37.2776
-
-#define BrakePin 33
-
-#define SpeedometerPin 32
-#define Circumference 455
-
-
-#define MotorPin1 27
-#define MotorPin2 14
-#define MotorPin3 12
-#define MotorPin4 13
-
-//I2C Addresses
-
-#define MPU6050Address 0x68
-
-#define HMC5883LAddress 0x0D
-
-#define BME280Address 0x76
-
-#define RXMinDuty 1000
-#define RXMaxDuty 2000
-
-#define MotorMinDuty 1000
-#define MotorMaxDuty 2000
-
-#define SteeringMinDuty 500
-#define SteeringMaxDuty 2500
-
-#define BrakeMinDuty 1000
-#define BrakeMaxDuty 2000
-
-#define MaxSpeed 20000
+#include <Definition.h>
 
 volatile unsigned long last_time = 0;
 volatile double speed = 0;
@@ -261,7 +199,7 @@ void safeMode(){
 
   for(;;){
     if(triggered){
-      if(PulseWidth2<20){
+      if(PulseWidth4<20 && PulseWidth3<5){
         if((millis()-trig_time) > 3000){
           Serial.println("Vehicle Armed!!!");
           delay(200);
@@ -273,7 +211,7 @@ void safeMode(){
       }
     }
     if(!triggered){
-      if(PulseWidth2<20){
+      if(PulseWidth4<20 && PulseWidth3<5){
         triggered = true;
         trig_time = millis();
         Serial.println("Triggered");
